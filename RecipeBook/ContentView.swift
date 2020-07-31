f9f9ecbdd68f76abdb4f5e6b24d38b9e6174efc3
NormandM <nmartin1956@gmail.com>
2020-07-31 15:37:29 -0400

    Initial Commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by Normand Martin on 2020-07-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
